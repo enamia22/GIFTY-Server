876f03a23f2c9a7f91e5948fcded315cadb7d677
mustapha ettanti <mustaphaettanti@gmail.com>
2023-10-17 11:30:00 +0100

    login api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 router.post("/", userController.addUser);
+router.post("/login", userController.loginUser);
 router.get("/", (req, res) => {
     res.send("hello")
 });
