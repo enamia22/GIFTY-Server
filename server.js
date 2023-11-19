95870bc6028f3a56ee73c9927d38acb86f73f6bb
mustapha ettanti <mustaphaettanti@gmail.com>
2023-11-19 12:16:14 +0100

    fixing products issues

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: 'http://localhost:3000',
   credentials: true,
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
